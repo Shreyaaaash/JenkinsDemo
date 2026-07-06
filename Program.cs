c6ed3ba3be341132ffdbf17f5862b4fe90f10183
Shreyaaaash <iamshreyash.nikam@gmail.com>
2026-07-06 11:56:14 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Welcome to DevOps Practice!");
+
+Console.WriteLine($"Current Time: {DateTime.Now}");
+
+Console.WriteLine($"Machine Name: {Environment.MachineName}");
+
+Console.WriteLine($"OS Version: {Environment.OSVersion}");
